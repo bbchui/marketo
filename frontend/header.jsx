648d910db3d885fd716835481f1f558449ca1f34
Brandon Chui <bbchui325@gmail.com>
2018-05-17 14:26:28 -0700

    minor fixes to header

--- frontend/header.jsx
+++ frontend/header.jsx
@@ -7,8 +7,19 @@ class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {visible: '',
-                  extra: 'extra'};
+                  scrolled: ''};
     this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', () => {
+      console.log(window.pageYOffset);
+      if (window.pageYOffset !== 0) {
+        this.setState({scrolled: 'scrolled'})
+      } else {
+        this.setState({scrolled: ''})
+      }
+    })
   }
 
   handleClick(e) {
@@ -25,7 +36,7 @@ class Header extends React.Component {
   render() {
     return(
       <header>
-        <nav>
+        <nav id="nav" className={this.state.scrolled}>
           <a className="logo" href="#">
             <img className="logo" src={marketo_logo} />
           </a>
@@ -37,20 +48,23 @@ class Header extends React.Component {
               <a href="#login">LOGIN</a>
             </div>
             <div className={'menu ' + this.state.visible} id="sidebar">
+              <a className="extra" href="#home" id="extra-logo">
+                <img src={marketo_logo} />
+              </a>
               <a href="#solutions" className="menu-item">Solutions</a>
               <a href="#my_resources" className="menu-item">My Resources</a>
               <a href="#why_marketo" className="menu-item">Why Marketo</a>
               <a href="#view-demo" className="view-demo">VIEW DEMO <FontAwesomeIcon icon="play-circle" className="play-circle"/></a>
-              <a className={this.state.extra} href="#login">Login</a>
-              <a className={this.state.extra} href="#contact">Contact</a>
-
+              <a className="extra" href="#login">Login</a>
+              <a className="extra" href="#contact">Contact</a>
+              <div id="sidebar-btn" onClick={this.handleClick}>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
             </div>
           </section>
-          <div id="sidebar-btn" onClick={this.handleClick}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
+
         </nav>
         <section className="header-titles">
           <h2 className="extra2">Marketo University</h2>
